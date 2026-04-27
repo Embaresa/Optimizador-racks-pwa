3b2083ea159030885e6f42bc1af8b1acff1c2045
Embaresa <ssantos@embaresa.com>
2026-04-27 17:33:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — cache offline-first
 // Incrementar CACHE_VERSION sempre que alterar qualquer ficheiro estático
 
-const CACHE_VERSION = 'v46';
+const CACHE_VERSION = 'v48';
 const CACHE_NAME = `racks-pwa-${CACHE_VERSION}`;
 
 // Ficheiros locais a pré-cachear
